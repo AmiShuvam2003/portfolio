def6caa5621bde0db0466237b845850050db3f43
Shuvam Samanta <161609142+AmiShuvam2003@users.noreply.github.com>
2025-06-17 13:03:40 +0530

    new commit

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-document.getElementById("toggle-theme").addEventListener("click", () => {
-  document.body.classList.toggle("dark-theme");
-});
+window.onload = function () {
+  console.log("Page is fully loaded");
 
 document.getElementById("contact-form").addEventListener("submit", function (e) {
   e.preventDefault();
   alert("Thanks for your message! 🚀 I’ll get back to you soon.");
   this.reset();
 });
+
+  const profileToggle = document.getElementById("profileToggle");
+  const dropdownMenu = document.getElementById("dropdownMenu");
+
+  console.log("profileToggle:", profileToggle);
+  console.log("dropdownMenu:", dropdownMenu);
+
+  profileToggle.addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdownMenu.style.display =
+      dropdownMenu.style.display === "block" ? "none" : "block";
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!profileToggle.contains(e.target)) {
+      dropdownMenu.style.display = "none";
+    }
+  });
+};
