c69e9016427058edc81600419502f62e6336df32
Shuvam Samanta <161609142+AmiShuvam2003@users.noreply.github.com>
2025-06-17 11:14:22 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("toggle-theme").addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for your message! 🚀 I’ll get back to you soon.");
+  this.reset();
+});
