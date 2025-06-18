a70cd7ce370d051373a465351cd64ce94edf76c6
Shuvam Samanta <161609142+AmiShuvam2003@users.noreply.github.com>
2025-06-18 16:22:08 +0530

    commit

---
+++ config.js
@@ -0,0 +1,6 @@
+const EMAILJS_CONFIG = {
+  PUBLIC_KEY: "Aq4ZRZYFsRoaozV9t",
+  SERVICE_ID: "service_m4cfkga",
+  TEMPLATE_ID: "template_hi6wkkb",
+  RECAPTCHA_SITE_KEY: "6LeW-2QrAAAAAG8yvrs_00_L6xeO1vMK43ME6sBb"
+};
